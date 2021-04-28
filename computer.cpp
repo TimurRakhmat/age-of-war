67d76db8721b7b1f22ae9004631dc706cbe0d991
TimurRakhmat <45202317+TimurRakhmat@users.noreply.github.com>
2021-04-29 01:27:02 +0300

    Add files via upload
    imrpove warrior class, add new unit type

---
+++ computer.cpp
@@ -0,0 +1,89 @@
+#include "computer.h"
+
+static int randomBetween(int low, int high)
+{
+    return (qrand() % ((high + 1) - low) + low);
+}
+
+int Cps = 3400;
+
+Computer::Computer() :
+    Player()
+{
+    money = 400;
+    warrior_cost = 40;
+    archer_cost = 55;
+    brain_timer = new QTimer();
+    connect(brain_timer, &QTimer::timeout, this, &Computer::brain_time_slot);
+    brain_timer->start(Cps);
+}
+
+Computer::~Computer()
+{
+
+}
+
+int Computer::type() const
+{
+    return Type;
+}
+
+
+void Computer::hit(int damage)
+{
+    health -= damage;   // Уменьшаем здоровье мишени
+    this->update(this->boundingRect());    // Перерисовываем мишень
+    // Если здоровье закончилось, то инициируем смерть мишени
+    if(health <= 0) this->deleteLater();
+}
+
+void Computer::stop()
+{
+    brain_timer->stop();
+    timer_update->stop();
+    timer_update_frame->stop();
+}
+
+void Computer::resume()
+{
+    brain_timer->start(Cps);
+    timer_update->start(cps);
+    timer_update_frame->start(Fps);
+}
+
+void Computer::brain_time_slot()
+{
+    int ch = randomBetween(1, 3);
+
+    if (ch < 3)
+        set_warrior();
+    else
+        set_archer();
+}
+
+void Computer::set_warrior()
+{
+    if (money < warrior_cost)
+        return;
+    money -= warrior_cost;
+    Warrior* wars = new Skelet_rigth();
+    connect(timer_update, &QTimer::timeout, wars, &Warrior::update_state);
+    connect(timer_update_frame, &QTimer::timeout, wars, &Warrior::nextFrame);
+    connect(wars, &Warrior::drop_money, this, &Player::unit_drop_money);
+    wars->setPos(x() - 20, y() + 185);
+    scene()->addItem(wars);
+}
+
+void Computer::set_archer()
+{
+    if (money < warrior_cost)
+        return;
+    money -= archer_cost;
+    Warrior* wars = new Archer_right();
+    connect(timer_update, &QTimer::timeout, wars, &Warrior::update_state);
+    connect(timer_update_frame, &QTimer::timeout, wars, &Warrior::nextFrame);
+    connect(wars, &Warrior::drop_money, this, &Player::unit_drop_money);
+    wars->setPos(x() - 20, y() + 185);
+    scene()->addItem(wars);
+}
+
---
+++ warrior.h
@@ -0,0 +1,138 @@
+#ifndef WARRIOR_H
+#define WARRIOR_H
+
+#include <QDebug>
+#include <QObject>
+#include <QGraphicsItem>
+#include <QTimer>
+#include <QGraphicsScene>
+#include <QPixmap>
+#include <QPainter>
+#include "effects.h"
+#include "player.h"
+
+
+class Warrior : public QObject, public QGraphicsItem
+{
+    Q_OBJECT
+public:
+    explicit Warrior(QObject *parent = 0);
+    ~Warrior();
+
+    virtual int type() const = 0;
+    void hit(int);
+
+signals:
+    void drop_money(int);
+
+public slots:
+    virtual void update_state() = 0;
+    void nextFrame();
+
+private:
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+    QRectF boundingRect() const;
+
+protected:
+    int health;
+    int maxHealth;
+
+    int money_drop;
+
+    int frame_step;
+
+    QPixmap *walk = nullptr;
+    QPixmap *atack = nullptr;
+    QPixmap *idle = nullptr;
+    QPixmap *dead = nullptr;
+    QPixmap *thatFrame = walk;
+
+    int dx, dy;
+
+    int speed;
+    int dmg;
+    int frameHeight;
+    int scale;
+    int currentFrame;
+};
+
+
+class Skelet_rigth : public Warrior
+{
+    Q_OBJECT
+public:
+    explicit Skelet_rigth();
+    int type() const;
+
+    enum { Type = UserType + 2 };
+
+public slots:
+    void update_state();
+};
+
+
+class Skelet_left : public Warrior
+{
+    Q_OBJECT
+public:
+    explicit Skelet_left();
+    int type() const;
+    enum { Type = UserType + 1 };
+
+public slots:
+    void update_state();
+};
+
+
+class Archer_left : public Warrior
+{
+    Q_OBJECT
+public:
+    explicit Archer_left();
+    int type() const;
+    enum { Type = UserType + 1 };
+
+public slots:
+    void update_state();
+
+};
+
+
+class Archer_right : public Warrior
+{
+    Q_OBJECT
+public:
+    explicit Archer_right();
+    int type() const;
+    enum { Type = UserType + 2 };
+
+public slots:
+    void update_state();
+};
+
+
+class Knight_left : public Warrior
+{
+    Q_OBJECT
+public:
+    explicit Knight_left();
+    int type() const;
+    enum { Type = UserType + 1 };
+
+public slots:
+    void update_state();
+};
+
+class Knight_right : public Warrior
+{
+    Q_OBJECT
+public:
+    explicit Knight_right();
+    int type() const;
+    enum { Type = UserType + 1 };
+
+public slots:
+    void update_state();
+};
+
+#endif // WARRIOR_H
